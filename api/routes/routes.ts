9a9f0521f17790a6a0600d21a9daf2067edfd2e5
Gabriel Froes <gabriel@rwstudio.com.br>
2020-06-12 10:12:30 -0300

    Refatoring JS to TypeScript

--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -1,18 +1,8 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import {
-  getChapters,
-  getChapter,
-  addChapter,
-  updateChapter,
-  deleteChapter,
-} from "../controllers/youtube-chapters-controller.ts";
+import { getChapters } from "../controllers/youtube-chapters-controller.ts";
 
 const router = new Router();
 
-router.get("/api/v1/video-chapters/:id", getChapters)
-  .get("/api/v1/video-chapters/:id/:seconds", getChapter)
-  .post("/api/v1/video-chapters/:id", addChapter)
-  .put("/api/v1/video-chapters/:id/:seconds", updateChapter)
-  .delete("/api/v1/video-chapters/:id/:seconds", deleteChapter);
+router.get("/api/v1/video-chapters/:id", getChapters);
 
 export default router;
