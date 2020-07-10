c82bffe141b6b487e891973f60f66c4defcf5823
Gabriel Froes <gabriel@rwstudio.com.br>
2020-07-10 10:56:04 -0300

    CORS Issue

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,14 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import router from "./routes/routes.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const HOST = config().HOST ?? "127.0.0.1";
 const PORT = config().PORT ?? 8080;
 
 const app = new Application();
 
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
